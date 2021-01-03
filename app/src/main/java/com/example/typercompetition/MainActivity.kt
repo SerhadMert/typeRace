711b0e85ce2e24a616a492b87e1bb8af0238466a
Kaan Sarıkaya <kadirkaansarikaya@gmail.com>
2021-01-03 15:07:16 +0300

    first commit

--- app/src/main/java/com/example/typercompetition/MainActivity.kt
+++ app/src/main/java/com/example/typercompetition/MainActivity.kt
@@ -19,8 +19,21 @@ class MainActivity : AppCompatActivity() {
 
         val editText = findViewById<EditText>(R.id.compare_text)
         val scoreText = findViewById<TextView>(R.id.score)
+        val mainText = findViewById<TextView>(R.id.main_text)
 
-        var textCompare: String = "selam"
+        var textCompare: String = "Lorem ipsum dolor sit amet consectetur adipiscing elit Nullam cursus nisl quis tortor aliquam vitae posuere risus lobortis " +
+                "Phasellus sed augue neque Praesent semper tortor lorem ac aliquet nisl vulputate ut Duis volutpat condimentum nunc a commodo " +
+                "Praesent venenatis metus lorem, et interdum risus tincidunt eu. Nullam fringilla vel tortor id auctor " +
+                "Maecenas egestas nulla at consectetur posuere Integer eget lectus et lacus imperdiet fringilla Donec ut ex in erat consequat auctor " +
+                "Aliquam metus lorem tristique non metus ac laoreet sodales quam Phasellus hendrerit lorem justo eget scelerisque arcu posuere ac " +
+                "Integer porta dui non mattis iaculis arcu enim convallis"
+
+        mainText.text = textCompare
+
+        var strs = textCompare.split(" ").toTypedArray()
+
+        mainText.text = strs.joinToString().replace(",", "")
+
 
 
 
@@ -32,21 +45,24 @@ class MainActivity : AppCompatActivity() {
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
                 var text = editText.text
-                //println(text.last())
-                var isWhitespace = text.contains(" ")
-                if (isWhitespace) {
-                    text = text.dropLast(1) as Editable?
-                    println("true")
-                    println("-" + text + "-")
-                    if (text.toString() == textCompare) {
-                        trueWord++
-                        editText.setText("")
-                    } else {
-                        falseWord++
+                if (strs.size > 0) {
+                    var isWhitespace = text.contains(" ")
+                    if (isWhitespace) {
+                        text = text.dropLast(1) as Editable?
+                        println("true")
+                        println("-" + text + "-")
+                        if (text.toString() == strs[0]) {
+                            trueWord++
+                            editText.setText("")
+                        } else {
+                            falseWord++
+                            editText.setText("")
+                        }
+                        strs = strs.drop(1).toTypedArray()
+                        mainText.text = strs.joinToString().replace(",", "")
+                        scoreText.text = "Doğru " + trueWord + "-Yanlış " + falseWord
+
                     }
-
-                    scoreText.text = "Doğru " + trueWord + "-Yanlış " + falseWord
-
                 }
 
             }
