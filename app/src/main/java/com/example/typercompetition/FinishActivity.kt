65c3585e9795cffb5c6a0ff811f7f0787757318e
Kaan Sarıkaya <kadirkaansarikaya@gmail.com>
2021-01-14 14:53:27 +0300

    Serhad bug fix v2 commit

--- app/src/main/java/com/example/typercompetition/FinishActivity.kt
+++ app/src/main/java/com/example/typercompetition/FinishActivity.kt
@@ -2,12 +2,9 @@ package com.example.typercompetition
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.CountDownTimer
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
 
 
 class FinishActivity : AppCompatActivity() {
@@ -17,14 +14,15 @@ class FinishActivity : AppCompatActivity() {
 
         val textView = findViewById<TextView>(R.id.crd1_text1)
         val textView2 = findViewById<TextView>(R.id.crd1_text2)
-        val trueWord = Integer.parseInt(intent.getStringExtra(SCORE_MESSAGE_T))
-        val falseWord = Integer.parseInt(intent.getStringExtra(SCORE_MESSAGE_F))
-
         val button1 = findViewById<Button>(R.id.button1)
         val button2 = findViewById<Button>(R.id.button2)
 
+
+        val trueWord = Integer.parseInt(intent.getStringExtra(SCORE_MESSAGE_T))
+        val falseWord = Integer.parseInt(intent.getStringExtra(SCORE_MESSAGE_F))
+
         textView.text = "D=" + trueWord + "Y=" + falseWord
-        textView2.text="SKOR = " + (trueWord - falseWord)
+        textView2.text = "SKOR = " + (trueWord - falseWord)
 
         button1.setOnClickListener {
             val intent = Intent(this@FinishActivity, GameActivity::class.java)
@@ -32,12 +30,14 @@ class FinishActivity : AppCompatActivity() {
             finish()
         }
         button2.setOnClickListener {
-            val intent = Intent(this@FinishActivity, MainActivity::class.java )
+            val intent = Intent(this@FinishActivity, MainActivity::class.java)
             startActivity(intent)
             finish()
         }
 
-        val timer = object : CountDownTimer(10000, 1000) {
+
+        //TİMERDAN VAZGEÇTİK AMA YİNE DE EKLEYEBİLİRİZ
+        /*val timer = object : CountDownTimer(10000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 // TODO
                 //      TEXTVİEW KOY ONA EŞİTLE= (millisUntilFinished/1000).toString()
@@ -45,12 +45,12 @@ class FinishActivity : AppCompatActivity() {
 
             override fun onFinish() {
                 //BURAYA DOKUNMA SAAT BİTİMİ BURASI
-                /* val intent = Intent(this@FinishActivity, MainActivity::class.java)
+                *//* val intent = Intent(this@FinishActivity, MainActivity::class.java)
                  startActivity(intent)
-                 finish()*/
+                 finish()*//*
             }
         }
-        timer.start()
+        timer.start()*/
 
     }
 }
