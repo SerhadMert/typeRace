4d2bca0040ac868256a522aaed2f88e2f24e1bd7
MiraiNatsu <1301smert@gmail.com>
2021-02-16 17:22:47 +0300

    MainActivity.kt settings pop-up.v1

--- app/src/main/java/com/example/typerace/MainActivity.kt
+++ app/src/main/java/com/example/typerace/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.typerace
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.view.animation.LinearInterpolator
 import android.widget.Button
 import android.widget.ImageButton
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-
+import kotlinx.android.synthetic.main.activity_settings.view.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,7 +18,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var btGame: Button
     lateinit var btGlobal: Button
     lateinit var btProfile: ImageButton
-
+    lateinit var btSettings: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +31,7 @@ class MainActivity : AppCompatActivity() {
         btGame = findViewById(R.id.btGame)
         btGlobal = findViewById(R.id.btGlobal)
         btProfile = findViewById(R.id.btProfile)
+        btSettings = findViewById(R.id.btSettings)
 
         Thread {
             try {
@@ -56,10 +59,15 @@ class MainActivity : AppCompatActivity() {
                         .setInterpolator(LinearInterpolator())
                         .setStartDelay(500)
                         .start()
+                btSettings.animate()
+                        .translationY(0f)
+                        .setInterpolator(LinearInterpolator())
+                        .setStartDelay(500)
+                        .start()
                 btGame.visibility = View.VISIBLE
                 btGlobal.visibility = View.VISIBLE
                 btProfile.visibility = View.VISIBLE
-
+                btSettings.visibility =View.VISIBLE
 
                 btGame.setOnClickListener(object : View.OnClickListener {
                     override fun onClick(view: View?) {
@@ -70,6 +78,21 @@ class MainActivity : AppCompatActivity() {
                 btProfile.setOnClickListener(object : View.OnClickListener{
                     override fun onClick(v: View?) {
                         startActivity(Intent(this@MainActivity, ProfileActivity::class.java))
+                    }
+
+                })
+                btSettings.setOnClickListener(object : View.OnClickListener{
+                    override fun onClick(v: View?) {
+                        val mDialogView = LayoutInflater.from(this@MainActivity).inflate(R.layout.activity_settings, null)
+                        val mBuilder = AlertDialog.Builder(this@MainActivity)
+                                .setView(mDialogView)
+                                .setTitle("")
+
+                        val  mAlertDialog = mBuilder.show()
+
+                        mDialogView.button_cikis.setOnClickListener {
+                            mAlertDialog.dismiss()
+                        }
                     }
 
                 })
